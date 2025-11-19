3f3f9fda3a7230253cd8dea60466334b4f6444b6
Dany Mcdonald <2561591@etudiant.cegepvicto.ca>
2025-11-19 11:47:51 -0500

    question 1 version 1.0

---
+++ assets/js/examen.js
@@ -0,0 +1,19 @@
+// =========================================================================
+// Déclaration des variables                                               =
+// =========================================================================
+const REGEX_CODE_COURS = /\d{3}-\d\w\d-\w{2}/;
+
+const messagesErreur = [
+    "Code invalide. Le code doit contenir exactement 10 caractères",
+    "Code invalide. Celui-ci être au format xxx-xAx-AA",
+    "Code non trouvée", 
+]
+
+const cours = [
+    { codeCours : "420-1E6-VI", nomCours : "Design Web"},
+    { codeCours : "420-1D6-VI", nomCours : "Programmation 1"},
+]
+
+// =========================================================================
+// Ajoutez votre code plus bas                                             =
+// =========================================================================
